bce21b1160372963e45037196be016b22d731b23
rick2046 <38879578+rick2046@users.noreply.github.com>
2020-08-24 15:37:44 +0800

    Update diy-x86-Lienol.sh
    fix some

--- diy-x86-Lienol.sh
+++ diy-x86-Lienol.sh
@@ -16,12 +16,12 @@ echo '修改时区'
 sed -i "s/'UTC'/'CST-8'\n        set system.@system[-1].zonename='Asia\/Shanghai'/g" package/base-files/files/bin/config_generate
 
 #echo '修改机器名称'
-#sed -i 's/OpenWrt/OpenWrt-x64/g' package/base-files/files/bin/config_generate
+#sed -i 's/OpenWrt/OpenWrt-x64-For-Rick/g' package/base-files/files/bin/config_generate
 
 echo '修改banner'
 cp -f diy/sypopo/banner package/base-files/files/etc/
 date=`date +%m.%d.%Y`
-sed -i "s/SyPopo$/SyPopo $date/g" package/base-files/files/etc/banner
+sed -i "s/SyPopo$/RickTse $date/g" package/base-files/files/etc/banner
 
 echo '添加软件包'
 #sed -i 's/0.32.1/0.33.0/g' package/diy/frp/Makefile
